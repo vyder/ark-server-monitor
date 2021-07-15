48a747f7db0cb4e4e61d36208b5c47b63413288e
Vidur Murali <vidur.murali@gmail.com>
2021-07-15 21:26:38 +0530

    Make discord messages rich text

--- monitor.js
+++ monitor.js
@@ -2,7 +2,7 @@ require('dotenv-yaml').config()
 const config = require('./config')
 const logger = require('./logger')(config)
 
-const { Webhook } = require('discord-webhook-node')
+const { Webhook, MessageBuilder } = require('discord-webhook-node')
 const hook = new Webhook(config.HOOK_URL)
 
 const Gamedig  = require('gamedig');
@@ -13,15 +13,28 @@ let serverStatus = null
 
 logger.info(`Querying: ${config.HOST}:${config.PORT}...`)
 
-const tellDiscord = msg => {
+const notifyDiscord = ({ serverIsUp, state }) => {
     if (config.ENABLE_DISCORD) {
-        try {
-            // hook.send(msg)
-        } catch(error) {
-            logger.error(`DISCORD:`, error)
+        const message = new MessageBuilder()
+
+        if (!serverIsUp) {
+            message.setTitle('Server is offline')
+            message.setColor("#38A169")
+
+        } else {
+            message.setTitle('Server is online')
+            message.setColor("#E53E3E")
+            message.addField(`${state.name} [${state.map}]`, state.connect)
+            message.addField("Ping", state.ping)
         }
+
+        hook.send(message)
+        .then(() => logger.info("Discord Notification sent"))
+        .catch(error => logger.error("Discord Error: ", JSON.stringify(error, null, 2)))
+
+    } else {
+        logger.info(`ENABLE_DISCORD: false, serverIsUp: ${serverIsUp}, state: ${JSON.stringify(state, null, 2)}`)
     }
-    logger.info(`DISCORD: ${msg}`)
 }
 
 const query = () => Gamedig.query({ type: 'arkse', host: config.HOST, port: config.PORT })
@@ -29,15 +42,15 @@ const query = () => Gamedig.query({ type: 'arkse', host: config.HOST, port: conf
         logger.info(`Server is online: ${state.name}`)
         if (!serverStatus) {
             logger.info(`Notify...`)
-            tellDiscord(`Server is online: ${state.name}`)
+            notifyDiscord({ serverIsUp: true, state })
             serverStatus = state
         }
     })
     .catch(error => {
-        logger.info("Server is offline")
+        logger.info("Server is offline", JSON.stringify(error, null, 2))
         if (serverStatus) {
             logger.info(`Notify...`)
-            tellDiscord(`Server is offline`)
+            notifyDiscord({ serverIsUp: false })
             serverStatus = null
         }
     })
